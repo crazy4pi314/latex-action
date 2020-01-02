e468183610c683edf3acddbffe3ae66da21de76b
Sarah Kaiser <sarah.kaiser@pensardevelopment.com>
2020-01-01 18:22:41 -0800

    making sure error codes from latexmk are caught

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -4,13 +4,44 @@ param(
     $Engine
 )
 
+function Write-ActionCommand {
+    param(
+        [string] $CommandName,
+        [hashtable] $Args,
+        [string] $Message
+    );
+
+    $ArgsAsString = (
+        $Args.GetEnumerator()
+        | ForEach-Object {
+            "$($_.Key)=$($_.Value)"
+        }
+    ) -join ",";
+    Write-Host "::$CommandName $ArgsAsString::$Message";
+
+}
+
 function Set-Output {
     param(
         $Name,
         $Value
     );
-    Write-Host "::set-output name=$Name::$Value";
+    Write-ActionCommand "set-output" -Args @{"Name" = $Value}
+}
 
+function Write-ActionError {
+    param(
+        $Message,
+        $File = $null,
+        $Line = $null,
+        $Column = $null
+    );
+
+    $Args = @{}
+    if ($null -ne $File) { $Args["file"] = $File; }
+    if ($null -ne $Line) { $Args["line"] = $Line; }
+    if ($null -ne $Column) { $Args["col"] = $Column; }
+    Write-ActionCommand "error" -Message $Message -Args $Args;
 }
 
 $ExtraArgs = @()
@@ -24,3 +55,7 @@ if ("xelatex" -eq $Engine) {
 Set-Output -Name time -Value (Get-Date)
 
 latexmk @ExtraArgs $FileName
+$LatexmkExitCode = $LASTEXITCODE
+if ($LatexmkExitCode -ne 0) {
+    Write-ActionError "latexmk returned exit code $LatexmkExitCode. Check logs for details.";
+}
